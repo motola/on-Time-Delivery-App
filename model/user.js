f539ec62caad2cffc1ad460656a2bf2438b899a1
motola <olutolamoses@gmailcom>
2022-06-26 22:16:58 +0100

    putting up major endpoints

---
+++ model/user.js
@@ -0,0 +1,21 @@
+let user = [
+    {
+     id: 1,
+     firstName: 'Andrew',
+     lastName: 'Parker',
+     email: 'mide123@gmail.com',
+     password: 12345,
+
+    },
+    {
+        id: 2,
+        firstName: 'Toby',
+        lastName: 'Macguire',
+        email: 'mide123@gmail.com',
+        password: 12345,
+   
+       },
+
+];
+
+module.exports = { user }
